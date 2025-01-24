3bd47b8f17c1d8ca916b36ac4f688425680c724a
Trikzmr <mrtrikz729@gmail.com>
2025-01-24 10:29:56 +0530

    working in landing page

---
+++ Client/mentor connect/src/Pages/Landing/Landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from './Components/HeroSection'
+
+const Landing = () => {
+  return (
+    <>
+        <div className="sec">
+        <HeroSection/>
+        </div>
+        
+        <h1>Test</h1>
+    </>
+  )
+}
+
+export default Landing
