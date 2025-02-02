3ac250e2c3a2bc4f9aa03e4678bc71a259df8444
Trikzmr <mrtrikz729@gmail.com>
2025-02-02 11:22:38 +0530

    working in project details page

--- Client/mentor connect/src/Pages/Home/Components/MyProjectSection.jsx
+++ Client/mentor connect/src/Pages/Home/Components/MyProjectSection.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const MyProjectSection = () => {
   return (
     <div>
-      
+      My Project Section
     </div>
   )
 }
--- Client/mentor connect/src/Pages/Project/ProjectDetails.jsx
+++ Client/mentor connect/src/Pages/Project/ProjectDetails.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ProjectDetailsSection from './ProjectDetailsSection';
 
 const ProjectDetails = () => {
-  return (
-    <div>
-      Project Details
-    </div>
-  )
-}
+  
 
-export default ProjectDetails
+  return (
+    <div className="min-h-screen p-5">
+
+      {/* Parent flex container */}
+      <div className="flex flex-col md:flex-row gap-4">
+        {/* Left Section */}
+        <div className="md:w-2/3 flex justify-center items-center p-5 rounded-lg">
+          <ProjectDetailsSection/>
+        </div>
+
+        {/* Right Section */}
+        <div className="md:w-1/3 flex justify-center items-center p-5 rounded-lg">
+          <div className="JoinProjectSection">
+            <p>Join Project Section</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
