b9da77da043e05abed5daef5e172cf2228c092cf
Trikzmr <mrtrikz729@gmail.com>
2025-02-03 22:34:29 +0530

    woking in home page

--- Client/mentor connect/src/Pages/Home/Components/MyProjectSection.jsx
+++ Client/mentor connect/src/Pages/Home/Components/MyProjectSection.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import MyComponent from './MyComponent';
 
 const MyProjectSection = () => {
-  let key =0;
   const [myProjects, setMyProjects] = useState([]);
+  const [index, setIndex] = useState(1);
+  const [currentPage, setCurrentPage] = useState([]);
 
   const fetchProject = async () => {
     try {
@@ -16,7 +17,6 @@ const MyProjectSection = () => {
       if (response.ok) {
         const projects = await response.json();
         setMyProjects(projects);
-        console.log(projects);
       } else {
         console.error('Failed to fetch projects');
       }
@@ -29,12 +29,41 @@ const MyProjectSection = () => {
     fetchProject();
   }, []);
 
+  useEffect(() => {
+    const start = (index - 1) * 3;
+    setCurrentPage(myProjects.slice(start, start + 3));
+  }, [index, myProjects]);
+
+  const prev = () => {
+    if (index > 1) {
+      setIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const next = () => {
+    if (index * 3 < myProjects.length) {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
   return (
-    <div>
-     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 md:gap-x-10 mx-auto mt-8 ">
-        {myProjects.map((project) => (
+    <div className='mb-16'>
+      <div className=" mb-8">
+        <h1 className="text-3xl font-semibold text-gray-800">My Projects</h1>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 md:gap-x-10 mx-auto mt-8 ">
+        {currentPage.map((project) => (
           <MyComponent key={project._id} project={project} />
         ))}
+      </div>
+      <div className="pagination flex justify-center items-center mt-8">
+        <button className="button-secondary rounded-full p-2 px-6 mr-2 mb-2" onClick={prev} disabled={index === 1}>
+          Prev
+        </button>
+        <button className="button-secondary rounded-full p-2 px-6 mr-2 mb-2">{index}</button>
+        <button className="button-secondary rounded-full p-2 px-6 mr-2 mb-2" onClick={next} disabled={index * 3 >= myProjects.length}>
+          Next
+        </button>
       </div>
     </div>
   );
