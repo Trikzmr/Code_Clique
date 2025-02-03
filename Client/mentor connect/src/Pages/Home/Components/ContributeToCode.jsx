b9fff954b25160fd388d873ede178bd85fc1f62d
iamamansinghrajput <singhaman2321@gmail.com>
2025-02-03 12:32:28 +0530

    componenet

---
+++ Client/mentor connect/src/Pages/Home/Components/ContributeToCode.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContributeToCode = () => {
+  return (
+    <div className="ContributeToCode">
+        <button type='submit'> Join US</button>
+    </div>
+  )
+}
+
+export default ContributeToCode
