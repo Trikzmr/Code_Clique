fc6fd93ee66943a968cc5b178cc58dea7eb6e057
Trikzmr <mrtrikz729@gmail.com>
2025-02-20 22:45:19 +0530

    Working on request Card Ui

---
+++ Server/routes/GetUser.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../model/userModel')
+
+const apicreate = async(req, res) => {
+    
+    const {Username} = req.body;
+    try {
+        let finduser = await User.findOne({Username});
+        
+
+        res.status(201).json(finduser);
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({err: error});
+    }
+    
+}
+
+router.post('/GetUser', apicreate);
+
+module.exports = router;
