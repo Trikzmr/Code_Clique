e69f097975ecfaf99ff50bfb9b6d811734c84c1c
Trikzmr <mrtrikz729@gmail.com>
2025-04-01 10:02:17 +0530

    working

--- Client/mentor connect/src/Components/Router.jsx
+++ Client/mentor connect/src/Components/Router.jsx
@@ -4,7 +4,6 @@ import Home from '../Pages/Home/Home';  // Import the Home component
 import Login from '../Pages/Authentication/Login';
 import ProjectDashboard from '../Pages/Project/ProjectDashboard';  // Import the ProjectDashboard component
 import ProjectDetails from '../Pages/Project/ProjectDetails';
-import ProjectManagement from '../Pages/ProjectManagement/ProjectManagement'
 import Requestform from '../Pages/Home/Components/Requestform';
 import Profile from '../Pages/Profile/Profile';
 import Register from '../Pages/Authentication/Register';
