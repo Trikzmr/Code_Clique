635308d2b557814b2181458c0521fb51eda4a9bc
Trikzmr <mrtrikz729@gmail.com>
2025-04-21 23:21:16 +0530

    aa

--- Client/mentor connect/src/Pages/Authentication/Components/LoginArea.jsx
+++ Client/mentor connect/src/Pages/Authentication/Components/LoginArea.jsx
@@ -12,7 +12,7 @@ const LoginArea = () => {
   const apifetch = async() => {
 
     try {
-      const response = await fetch('http://localhost:3000/api/LoginUser', {
+      const response = await fetch('https://code-clique-9qgm.vercel.app/api/LoginUser', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
