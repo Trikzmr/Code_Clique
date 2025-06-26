32819b8f0e1b3131bdeb8173de14632a95d179d7
Trikzmr <mrtrikz729@gmail.com>
2025-06-26 20:57:50 +0530

    fixing server

--- Server/db/conn.js
+++ Server/db/conn.js
@@ -2,11 +2,24 @@ const mongoose = require("mongoose");
 
 const DB = "mongodb+srv://debabratodas729:qzqRYddVN5eFh72K@database1.u7jnn.mongodb.net/?retryWrites=true&w=majority&appName=Database1";
 
+let isConnected = false;
 
+async function connectDB() {
+  if (isConnected) {
+    return;
+  }
 
-mongoose.connect(DB, {})
-    .then(() => console.log("Connection successful"))
-    .catch((error) => {
-        console.error("MongoDB connection error:", error);
-        console.error("Cause:", error.cause);
+  try {
+    const db = await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+
+    isConnected = db.connections[0].readyState;
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+  }
+}
+
+module.exports = connectDB;
